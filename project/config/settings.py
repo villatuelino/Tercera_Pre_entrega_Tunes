4f19ae154951f6f635989216c20fbd87a9b1f5fa
Guillermo <guillermotunes@hotmail.com>
2023-06-03 05:45:29 -0300

    Creacion projecto y apps home

--- project/config/settings.py
+++ project/config/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+import sys
+
+APLICACIONES = BASE_DIR / "apps"
+sys.path.append(str(APLICACIONES))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
